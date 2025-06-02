5349717a4453bf6067e76a6d31d7e816b062248b
jb260225 <jbcompanyyy25@gmail.com>
2025-06-02 17:56:32 +0700

    Add files via upload

---
+++ story_content/user.js
@@ -0,0 +1,20 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "61bGYICx2tK":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  //menentukan variabel
+var audio = document.getElemenByld('bgSongku');
+audio.src="musikku.mp3";
+audio.load0;
+audio.play0;
+audio.volume=0,3;
+}
+
